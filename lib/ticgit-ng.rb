5d82afc4eb1715a04eea25296518b0b6821e2de0
Jeff Welling <jeff.welling@gmail.com>
2012-06-15 22:06:58 -0400

    Switch to RbConfig

--- lib/ticgit-ng.rb
+++ lib/ticgit-ng.rb
@@ -15,7 +15,7 @@ require 'rubygems'
 require 'git'
 
 #Only redefine if we are not using 1.9
-unless Config::CONFIG["ruby_version"][/^\d\.9/]
+unless RbConfig::CONFIG["ruby_version"][/^\d\.9/]
   # FIXME: Monkeypatch git until fixed upstream
   module Git
     class Lib
