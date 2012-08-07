c5650c5d0c302c7519ca8e3c0e5c8a3accbf0290
Jeff Welling <jeff.welling@gmail.com>
2012-08-06 22:41:59 -0400

    Fixed a spec, finished more cli specs
    - Fixed the 'it should list the tickets' spec, and
      finished the 'should show a ticket' and 'should
      return the same ticket from ticket_show and
      ticket_new

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -18,7 +18,8 @@ describe TicGitNG::CLI do
   it "should list the tickets" do
     @ticgitng.tickets.size.should eql(0)
     @ticgitng.ticket_new('my new ticket').should be_an_instance_of(TicGitNG::Ticket)
-    @ticgitng.tickets.size.should eql(1)
+    @ticgitng.ticket_new('my second ticket').should be_an_instance_of(TicGitNG::Ticket)
+    @ticgitng.tickets.size.should eql(2)
     fields = %w[TicId Title State Date Assgn Tags]
     output = []
     # It's unclear why it's necessary to append each line like this, but
@@ -30,12 +31,146 @@ describe TicGitNG::CLI do
     output.shift.should match ""
     output.shift.should match /#{fields.join( '\s+' )}/
     output.shift.should match /^-+$/
-    #check that at least a SHA1 prefix exists
+    #check that at least the SHA1 prefix exists
+    output.shift[/^[a-z0-9]{6}\s/].should_not == nil
     output.shift[/^[a-z0-9]{6}\s/].should_not == nil
     output.shift.should match ""
   end
 
-  it "should show a ticket"
+  it "should return the same ticket from ticket_show and ticket_new" do
+      #FIXME the output of cli(@path,'list') produces a random order
+      #- This has to do with a lack of timeskew
+      20.times do
+        #setup git repo
+        git_dir= setup_new_git_repo
+        base= TicGitNG.open( git_dir, test_opts )
+        base.tickets.size.should eql(0)
+
+        tic1=base.ticket_new('my new ticket', Hash.new, t1=time_skew)
+        tic1.should be_an_instance_of(TicGitNG::Ticket)
+        tic1.should == base.ticket_show( tic1.ticket_id )
+
+        tic2=base.ticket_new('my second ticket', Hash.new, t2=time_skew)
+        tic2.should be_an_instance_of(TicGitNG::Ticket)
+        tic2.should == base.ticket_show( tic2.ticket_id )
+
+        tic1.should_not == tic2
+        tic1.should_not == base.ticket_show( tic2.ticket_id )
+
+        tic1.title.should == 'my new ticket'
+        tic2.title.should == 'my second ticket'
+        base.tickets.size.should eql(2)
+
+        if t1>t2
+            t=tic2
+            tic2=tic1
+            tic1=t
+        end
+
+        fields = %w[TicId Title State Date Assgn Tags]
+        output = []
+        # It's unclear why it's necessary to append each line like this, but
+        # cli('list') would otherwise return nil. The spec helper probably
+        # needs some refactoring.
+        cli(git_dir, 'init','list') do |line|
+          output << line
+        end
+        output.shift.should match ""
+        output.shift.should match /#{fields.join( '\s+' )}/
+        output.shift.should match /^-+$/
+
+        #first ticket
+        line=output.shift.split(' ')
+        line[0].should match /^[a-z0-9]{6}/
+        #This doesn't hinge on t1<t2 because assigned will be
+        # the same for both tickets
+        a=tic1.assigned
+        if a.bytesize > 8
+            a="#{a[0, 7]}+"
+        end
+        line.pop.should == a
+        #FIXME if t1<t2, t1, else, t2
+        line.pop.should == tic1.opened.strftime('%m/%d')
+        line.pop.should == 'open'
+        #delete the sha1
+        line.shift
+        line.join(' ').should == tic1.title
+
+        #second ticket
+        line=output.shift.split(' ')
+        line[0].should match /^[a-z0-9]{6}/
+        a=tic2.assigned
+        if a.bytesize > 8
+            a="#{a[0, 7]}+"
+        end
+        line.pop.should == a
+        line.pop.should == tic2.opened.strftime('%m/%d')
+        line.pop.should == 'open'
+        #delete the sha1
+        line.shift
+        line.join(' ').should == tic2.title
+      end
+  end
+
+  it "should show a ticket" do
+    #FIXME needs time_sku to avoid random failures
+    @ticgitng.tickets.size.should eql(0)
+
+    tic1=@ticgitng.ticket_new('my new ticket', Hash.new, t1=time_skew)
+    tic1.should be_an_instance_of(TicGitNG::Ticket)
+    tic1=@ticgitng.ticket_show(tic1.ticket_id)
+
+    tic2=@ticgitng.ticket_new('my second ticket', Hash.new, t2=time_skew)
+    tic2.should be_an_instance_of(TicGitNG::Ticket)
+    tic2=@ticgitng.ticket_show(tic2.ticket_id)
+
+    @ticgitng.tickets.size.should eql(2)
+    if t1>t2
+        t=tic2
+        tic2=tic1
+        tic1=t
+    end
+
+    fields = %w[TicId Title State Date Assgn Tags]
+    output = []
+    # It's unclear why it's necessary to append each line like this, but
+    # cli('list') would otherwise return nil. The spec helper probably
+    # needs some refactoring.
+    cli(@path, 'init','list') do |line|
+      output << line
+    end
+    output.shift.should match ""
+    output.shift.should match /#{fields.join( '\s+' )}/
+    output.shift.should match /^-+$/
+
+    #first ticket
+    line=output.shift.split(' ')
+    line[0].should match /^[a-z0-9]{6}/
+    a=tic1.assigned
+    if a.bytesize > 8
+        a="#{a[0, 7]}+"
+    end
+    line.pop.should == a
+    line.pop.should == tic1.opened.strftime('%m/%d')
+    line.pop.should == 'open'
+    #delete the sha1
+    line.shift
+    line.join(' ').should == tic1.title
+
+    #second ticket
+    line=output.shift.split(' ')
+    line[0].should match /^[a-z0-9]{6}/
+    a=tic2.assigned
+    if a.bytesize > 8
+        a="#{a[0, 7]}+"
+    end
+    line.pop.should == a
+    line.pop.should == tic2.opened.strftime('%m/%d')
+    line.pop.should == 'open'
+    #delete the sha1
+    line.shift
+    line.join(' ').should == tic2.title
+  end
 
   it 'displays --help' do
     expected = format_expected(<<-OUT)
