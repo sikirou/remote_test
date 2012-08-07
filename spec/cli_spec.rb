fefc152b5c9d900434bf19eaf656c7e5e486abff
Jeff Welling <jeff.welling@gmail.com>
2012-08-06 21:42:06 -0400

    Cleanup cli spec after each spec run
    - use :each instead of :all so that the specs are cleaned up and reset
      at the end and beginning of each spec.

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -3,13 +3,13 @@ require File.dirname(__FILE__) + "/spec_helper"
 describe TicGitNG::CLI do
   include TicGitNGSpecHelper
 
-  before(:all) do
+  before(:each) do
     @path = setup_new_git_repo
     @orig_test_opts = test_opts
     @ticgitng = TicGitNG.open(@path, @orig_test_opts)
   end
 
-  after(:all) do
+  after(:each) do
     Dir.glob(File.expand_path("~/.ticgit-ng/-tmp*")).each {|file_name| FileUtils.rm_r(file_name, {:force=>true,:secure=>true}) }
     Dir.glob(File.expand_path("~/.ticgit/-tmp*")).each {|file_name| FileUtils.rm_r(file_name, {:force=>true,:secure=>true}) }
     Dir.glob(File.expand_path("/tmp/ticgit-ng-*")).each {|file_name| FileUtils.rm_r(file_name, {:force=>true,:secure=>true}) }
